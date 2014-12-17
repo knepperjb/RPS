524325411cddd2be5baec06868fcfb5e8563fa7b
James Knepper <knepperjb@gmail.com>
2014-12-17 22:19:02 +0000

    add rps.rb

---
+++ lib/rps.rb
@@ -0,0 +1,63 @@
+module Rps
+
+  def self.create_db_connection(dbname)
+    PG.connect(host: 'localhost', dbname: dbname)
+  end
+
+  def self.clear_db(db)
+    db.exec %q[
+      DELETE FROM users;
+      DELETE FROM api_keys;
+      DELETE FROM matches;
+      DELETE FROM bouts;
+    ]
+  end
+
+  def self.create_tables(db)
+    db.exec %q[
+      CREATE TABLE IF NOT EXISTS users (
+        id          SERIAL PRIMARY KEY,
+        username    VARCHAR UNIQUE,
+        password    VARCHAR
+        );
+      CREATE TABLE IF NOT EXISTS api_keys (
+        id          SERIAL PRIMARY KEY,
+        api_key     VARCHAR,
+        user_id     INTEGER REFERENCES users(id) ON DELETE CASCADE
+        );
+      CREATE TABLE IF NOT EXISTS matches (
+        id              SERIAL PRIMARY KEY,
+        challenger_id   INTEGER REFERENCES users(id) ON DELETE CASCADE,
+        contender_id    INTEGER REFERENCES users(id) ON DELETE CASCADE,
+        winner          INTEGER REFERENCES users(id) ON DELETE CASCADE
+        );
+      CREATE TABLE IF NOT EXISTS bouts (
+        id              INTEGER SERIAL PRIMARY KEY,
+        chal_choice     VARCHAR,
+        cont_choice     VARCHAR,
+        winner          VARCHAR,
+        round           INTEGER,
+        game_id         INTEGER
+        );
+      ]
+  end
+
+  def self.seed_db(db)
+    sql = %q[
+      INSERT INTO users (username, password) values ($1, $2)
+    ]
+    db.exec(sql, ['marco polo', 'mongolia'])
+    db.exec(sql, ['santa claus', 'hohoho'])
+    db.exec(sql, ['brian', 'orphan'])
+  end
+
+  def self.drop_tables(db)
+    db.exec %q[
+      DROP TABLE users CASCADE;
+      DROP TABLE matches CASCADE;
+      DROP TABLE bouts CASCADE;
+      DROP TABLE api_keys CASCADE;
+    ]
+  end
+  
+        
