5c50e18b4693a0c8571a878e231f60e15d6da251
James Knepper <knepperjb@gmail.com>
2014-12-19 17:15:22 +0000

    add bouts.js file

---
+++ public/js/bouts.js
@@ -0,0 +1,19 @@
+var source = $('#bouthistory-template').html();
+var template = Handlebars.compile(source);
+
+
+
+$('.match').on('click', function () {
+	var matchId = $(this).attr("id");
+	$.ajax({
+		type: "GET",
+		url: "/matches/" + matchId;
+	}).success(function (bouts) {
+		console.log(bouts);
+		bouts.forEach(function (bout) {
+			var boutsHtml = template(bout);	
+		});
+		$('#boutHistory').append(boutsHtml);
+
+	});
+});
